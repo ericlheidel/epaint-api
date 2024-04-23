253e78c7d2d467b8a3aaf3a9da7dd137d1659ef6
Eric Heidel <ericleeheidel@yahoo.com>
2024-04-23 14:35:47 -0500

    initial commit

--- seed_database.sh
+++ seed_database.sh
@@ -5,6 +5,6 @@ rm -rf ./epaintapi/migrations
 python3 manage.py migrate
 python3 manage.py makemigrations epaintapi
 python3 manage.py migrate epaintapi
-python3 manage.py loaddata users
-python3 manage.py loaddata tokens
+# python3 manage.py loaddata users
+# python3 manage.py loaddata tokens
 
