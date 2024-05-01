c7a50462256de73c8898dbcf0e058020d9363619
Eric Heidel <ericleeheidel@yahoo.com>
2024-05-01 09:19:36 -0500

    working on this test, it currently is failing tho it shouldnt

--- tests/payments_test.py
+++ tests/payments_test.py
@@ -24,7 +24,7 @@ class PaymentTests(APITestCase):
         self.token = json_response["token"]
         self.assertEqual(response.status_code, HTTP_201_CREATED)
 
-    def test_create_payment_type(self):
+        # Create payment
 
         url = "/payments"
 
@@ -37,3 +37,63 @@ class PaymentTests(APITestCase):
         self.client.credentials(HTTP_AUTHORIZATION="Token " + self.token)
         response = self.client.post(url, data, format="json")
         self.assertEqual(response.status_code, HTTP_201_CREATED)
+
+    def test_get_user_payments(self):
+
+        # Create a 2nd payment
+
+        url = "/payments"
+
+        data = {
+            "name": "Visa",
+            "acct_number": "1234-5678-8765-4321",
+            "ex_date": "2030-03-03",
+        }
+
+        self.client.credentials(HTTP_AUTHORIZATION="Token " + self.token)
+        response = self.client.post(url, data, format="json")
+        self.assertEqual(response.status_code, HTTP_201_CREATED)
+
+        # Get both created payments connected to the created user
+
+        url = "/payments"
+
+        response = self.client.get(url, None, format="json")
+        json_response = json.loads(response.content)
+        self.assertEqual(response.status_code, HTTP_200_OK)
+        self.assertEqual(len(json_response), 2)
+
+
+# ++  /$$$$$$$  /$$$$$$$   /$$$$$$  /$$   /$$ /$$$$$$$$ /$$   /$$
+# ++ | $$__  $$| $$__  $$ /$$__  $$| $$  /$$/| $$_____/| $$$ | $$
+# ++ | $$  \ $$| $$  \ $$| $$  \ $$| $$ /$$/ | $$      | $$$$| $$
+# ++ | $$$$$$$ | $$$$$$$/| $$  | $$| $$$$$/  | $$$$$   | $$ $$ $$
+# ++ | $$__  $$| $$__  $$| $$  | $$| $$  $$  | $$__/   | $$  $$$$
+# ++ | $$  \ $$| $$  \ $$| $$  | $$| $$\  $$ | $$      | $$\  $$$
+# ++ | $$$$$$$/| $$  | $$|  $$$$$$/| $$ \  $$| $$$$$$$$| $$ \  $$
+# ++ |_______/ |__/  |__/ \______/ |__/  \__/|________/|__/  \__/
+
+# def test_update_payment(self):
+
+#     # Update created payment
+#     url = "/payments/1"
+#     data = {
+#         "name": "Discover",
+#         "acct_number": "8765-4321-1234-5678",
+#         "ex_date": "2050-05-05",
+#     }
+
+#     self.client.credentials(HTTP_AUTHORIZATION="Token " + self.token)
+#     response = self.client.put(url, data, format="json")
+#     self.assertEqual(response.status_code, HTTP_204_NO_CONTENT)
+
+#     # Get payment and confirm updates
+
+#     url = "/payments/"
+
+#     response = self.client.get(url, None, format="json")
+#     json_response = json.loads(response.content)
+#     self.assertEqual(response.status_code, HTTP_200_OK)
+#     self.assertEqual(json_response["name"], "Discover")
+#     self.assertEqual(json_response["acct_number"], "8765-4321-1234-5678")
+#     self.assertEqual(json_response["ex_date"], "2050-05-05")
