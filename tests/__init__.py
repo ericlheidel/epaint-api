ebf363981411750ea8fbaea564d656e1f8a77987
Eric Heidel <ericleeheidel@yahoo.com>
2024-05-01 09:20:34 -0500

    commented out the import in test initpy

--- tests/__init__.py
+++ tests/__init__.py
@@ -2,7 +2,8 @@
 # from .orders_test import OrderTests
 from .paints_test import PaintTests
 from .painttypes_test import PaintTypeTests
+
+# from .payments_test import PaymentTests
 from .profiles_test import ProfileTests
 from .registers_test import RegistersTests
 from .sizes_test import SizeTests
-from .payments_test import PaymentTests
