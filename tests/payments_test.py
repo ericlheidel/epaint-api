05a84ae5c3f344745e5f0c114f86d465729fce00
Eric Heidel <ericleeheidel@yahoo.com>
2024-05-04 15:33:51 -0500

    added successful tests for get payments and delete a payment

--- tests/payments_test.py
+++ tests/payments_test.py
@@ -24,7 +24,7 @@ class PaymentTests(APITestCase):
         self.token = json_response["token"]
         self.assertEqual(response.status_code, HTTP_201_CREATED)
 
-    def test_create_payment_type(self):
+        # Create a payment
 
         url = "/payments"
 
@@ -37,3 +37,43 @@ class PaymentTests(APITestCase):
         self.client.credentials(HTTP_AUTHORIZATION="Token " + self.token)
         response = self.client.post(url, data, format="json")
         self.assertEqual(response.status_code, HTTP_201_CREATED)
+
+    def test_get_all_payment(self):
+
+        # Create a second payment
+
+        url = "/payments"
+
+        data = {
+            "name": "Visa",
+            "acct_number": "1234-5678-8765-4321",
+            "ex_date": "2030-03-03",
+        }
+
+        self.client.credentials(HTTP_AUTHORIZATION="Token " + self.token)
+        response = self.client.post(url, data, format="json")
+        self.assertEqual(response.status_code, HTTP_201_CREATED)
+
+        url = "/payments"
+
+        self.client.credentials(HTTP_AUTHORIZATION="Token " + self.token)
+
+        response = self.client.get(url, None, format="json")
+        json_response = json.loads(response.content)
+        self.assertEqual(response.status_code, HTTP_200_OK)
+        self.assertEqual(len(json_response), 2)
+
+    def test_delete_payment_type(self):
+
+        url_one = "/payments/1"
+        url_two = "/payments"
+
+        self.client.credentials(HTTP_AUTHORIZATION="Token " + self.token)
+        response = self.client.delete(url_one, None, format="json")
+        self.assertEqual(response.status_code, HTTP_204_NO_CONTENT)
+
+        self.client.credentials(HTTP_AUTHORIZATION="Token " + self.token)
+        response = self.client.get(url_two, None, format="json")
+        json_response = json.loads(response.content)
+        self.assertEqual(response.status_code, HTTP_200_OK)
+        self.assertEqual(len(json_response), 0)
