87994970af1f1241202187965b48b94d9a9f746f
Eric Heidel <ericleeheidel@yahoo.com>
2024-05-04 18:47:07 -0500

    added successful test to remove paint from cart

--- tests/carts_test.py
+++ tests/carts_test.py
@@ -149,3 +149,86 @@ class CartTests(APITestCase):
                 "number_of_items": 1,
             },
         )
+
+    def test_delete_paint_from_cart(self):
+
+        # When an order doesn't exist, /cart GET should create an order
+
+        self.client.credentials(HTTP_AUTHORIZATION="Token " + self.token)
+        response = self.client.get("/cart", None, format="json")
+        self.assertEqual(response.status_code, HTTP_201_CREATED)
+
+        # Get order to confirm it was created
+
+        self.client.credentials(HTTP_AUTHORIZATION="Token " + self.token)
+        response = self.client.get("/orders/1", None, format="json")
+        json_response = json.loads(response.content)
+        self.assertDictEqual(
+            json_response,
+            {
+                "id": 1,
+                "created_date": datetime.datetime.now().strftime("%Y-%m-%d"),
+                "purchase_date": None,
+                "user_id": 1,
+                "payment_type_id": None,
+                "payment": None,
+                "is_completed": False,
+                "items": [],
+                "total": 0,
+                "number_of_items": 0,
+            },
+        )
+
+        data = {
+            "paint_id": 1,
+            "size_id": 1,
+        }
+
+        self.client.credentials(HTTP_AUTHORIZATION="Token " + self.token)
+        response = self.client.post("/profile/cart", data, format="json")
+        json_response = json.loads(response.content)
+        self.assertEqual(response.status_code, HTTP_201_CREATED)
+
+        # Get cart to confirm it has a paint in it
+
+        self.client.credentials(HTTP_AUTHORIZATION="Token " + self.token)
+        response = self.client.get("/cart", None, format="json")
+        json_response = json.loads(response.content)
+        self.assertDictEqual(
+            json_response,
+            {
+                "id": 1,
+                "created_date": "2024-05-04",
+                "purchase_date": None,
+                "user_id": 1,
+                "payment_type_id": None,
+                "payment": None,
+                "is_completed": False,
+                "items": [
+                    {
+                        "id": 1,
+                        "paint": {
+                            "id": 1,
+                            "color": "color1",
+                            "paint_number": "1111",
+                            "image_one": None,
+                            "image_two": None,
+                            "hex": None,
+                            "rgb": None,
+                            "cmyk": None,
+                            "paint_type_id": 1,
+                            "paint_type": {"id": 1, "name": "name"},
+                        },
+                        "size": {"id": 1, "size": "400ml", "price": 9.99},
+                    }
+                ],
+                "total": 9.99,
+                "number_of_items": 1,
+            },
+        )
+
+        # Delete paint from cart (order)
+
+        self.client.credentials(HTTP_AUTHORIZATION="Token " + self.token)
+        response = self.client.delete("/cart/1", None, format="json")
+        self.assertEqual(response.status_code, HTTP_204_NO_CONTENT)
