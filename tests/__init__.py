ae41b9285360314e8a846f66a336815af5d6bd94
Eric Heidel <ericleeheidel@yahoo.com>
2024-05-04 13:34:12 -0500

    updated fixtures and seeded database

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,8 +1,9 @@
-# from .create_django_password import CreateDjangoPassword
+from .create_django_password import CreateDjangoPassword
+
 # from .orders_test import OrderTests
-from .paints_test import PaintTests
-from .painttypes_test import PaintTypeTests
-from .profiles_test import ProfileTests
-from .registers_test import RegistersTests
-from .sizes_test import SizeTests
-from .payments_test import PaymentTests
+# from .paints_test import PaintTests
+# from .painttypes_test import PaintTypeTests
+# from .profiles_test import ProfileTests
+# from .registers_test import RegistersTests
+# from .sizes_test import SizeTests
+# from .payments_test import PaymentTests
