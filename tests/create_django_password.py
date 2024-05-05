7566885a83f658072a5302c8490f0603f021e2f9
Eric Heidel <ericleeheidel@yahoo.com>
2024-05-05 16:09:34 -0500

    updated use password to be more robust to avoid chrome change password alert

--- tests/create_django_password.py
+++ tests/create_django_password.py
@@ -6,24 +6,8 @@ class CreateDjangoPassword(APITestCase):
 
     def test_create_django_password_bob(self):
 
-        password = "bob"
+        password = "ThisIsMyPassword1234554321!!"
 
         django_password = make_password(password)
 
-        print(f"bob = {django_password}")
-
-    def test_create_django_password_tim(self):
-
-        password = "tim"
-
-        django_password = make_password(password)
-
-        print(f"tim = {django_password}")
-
-    def test_create_django_password_tom(self):
-
-        password = "tom"
-
-        django_password = make_password(password)
-
-        print(f"tom = {django_password}")
+        print(f"pw = {django_password}")
