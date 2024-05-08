e493acf8f85b44956169c50b8d8e8e23f8a10e6b
Eric Heidel <ericleeheidel@yahoo.com>
2024-05-08 09:56:00 -0500

    added tests for attempting to register with existing email and/or username

--- tests/registers_test.py
+++ tests/registers_test.py
@@ -55,3 +55,73 @@ class RegistersTests(APITestCase):
         self.assertEqual(json_response["valid"], True)
         self.assertIsNotNone(json_response["token"])
         self.assertIsNotNone(json_response["id"])
+
+    def test_cant_register_existing_email(self):
+
+        # Create a user
+        url = "/register"
+        data = {
+            "username": "eric",
+            "password": "eric",
+            "email": "eric@ericheidel.com",
+            "address": "212 Broadway",
+            "phone_number": "423-555-1234",
+            "first_name": "Eric",
+            "last_name": "Heidel",
+        }
+        response = self.client.post(url, data, format="json")
+        json_response = json.loads(response.content)
+        self.token = json_response["token"]
+        self.assertEqual(response.status_code, HTTP_201_CREATED)
+
+        # Attempt to create a new user with the same email as the created user
+
+        url = "/register"
+        data = {
+            "username": "eric_number_2",
+            "password": "eric_number_2",
+            "email": "eric@ericheidel.com",
+            "address": "212 Different Road",
+            "phone_number": "555-555-5555",
+            "first_name": "Eric Number 2",
+            "last_name": "Heidel Number 2",
+        }
+        response = self.client.post(url, data, format="json")
+        json_response = json.loads(response.content)
+        self.assertEqual(response.status_code, HTTP_400_BAD_REQUEST)
+        self.assertEqual(json_response["message"], "Email already exists")
+
+    def test_cant_register_user_existing_username(self):
+
+        # Create a user
+        url = "/register"
+        data = {
+            "username": "eric",
+            "password": "eric",
+            "email": "eric@ericheidel.com",
+            "address": "212 Broadway",
+            "phone_number": "423-555-1234",
+            "first_name": "Eric",
+            "last_name": "Heidel",
+        }
+        response = self.client.post(url, data, format="json")
+        json_response = json.loads(response.content)
+        self.token = json_response["token"]
+        self.assertEqual(response.status_code, HTTP_201_CREATED)
+
+        # Attempt to create a new user with the same username as the created user
+
+        url = "/register"
+        data = {
+            "username": "eric",
+            "password": "eric_number_2",
+            "email": "eric@ericheidel_number_2.com",
+            "address": "212 Different Road",
+            "phone_number": "555-555-5555",
+            "first_name": "Eric Number 2",
+            "last_name": "Heidel Number 2",
+        }
+        response = self.client.post(url, data, format="json")
+        json_response = json.loads(response.content)
+        self.assertEqual(response.status_code, HTTP_400_BAD_REQUEST)
+        self.assertEqual(json_response["message"], "Username already exists")
