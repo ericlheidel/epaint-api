76099cab5a3ec09ead7f42a653eb2e69770c8366
Eric Heidel <ericleeheidel@yahoo.com>
2024-05-15 12:31:06 -0500

    working on deployment

--- epaintproject/settings.py
+++ epaintproject/settings.py
@@ -1,3 +1,5 @@
+import os
+from django.core.management.utils import get_random_secret_key
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -8,12 +10,21 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-x9yg09-pv69(#mz@!n(1&c_rxvks#3*v&#vx!%t39p(n(f0gbb"
+# SECRET_KEY = "django-insecure-x9yg09-pv69(#mz@!n(1&c_rxvks#3*v&#vx!%t39p(n(f0gbb"
+#! deployment
+SECRET_KEY = os.getenv("DJANGO_SECRET_KEY", get_random_secret_key())
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+# DEBUG = True
+#! deployment
+DEBUG = os.getenv("DEBUG", "False")
 
-ALLOWED_HOSTS = []
+# ALLOWED_HOSTS = []
+#! deployment
+ALLOWED_HOSTS = os.getenv("DJANGO_ALLOWED_HOSTS", "").split(",")
+
+#! deployment
+DEVELOPMENT_MODE = os.getenv("DEVELOPMENT_MODE", "False")
 
 
 # Application definition
@@ -48,6 +59,7 @@ CORS_ORIGIN_WHITELIST = (
     "http://127.0.0.1:5173",
     "http://localhost:5174",
     "http://127.0.0.1:5174",
+    "https://paintkillerz.netlify.app",
 )
 
 
